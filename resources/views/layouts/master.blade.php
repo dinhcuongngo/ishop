b4175dd9ba576451f57239d0950a2909d067a893
Cuong Ngo <dinhcuongngo@gmail.com>
2018-04-27 17:27:43 +0700

    Uploaded Frontend

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="vn">
+<head>
+	<title>iShop::Home</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/normalize.css" >
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+</head>
+<body>
+<!-- START OF HEADER -->
+<header class="header">
+	<div class="container clearfix">
+		<div class="header__left">
+			<a href="" class="header__left_logo">
+				<i class="fab fa-skyatlas fa-lg"></i>
+			</a>
+		</div>
+		<div class="header__right">
+			<nav class="header__right_menu">
+				<ul>
+					<li><a href="/home">Home</a></li>
+					<li><a href="/users">User</a></li>
+					<li><a href="/shops">Shop</a></li>
+					<li><a href="/categories">Category</a></li>
+					<li><a href="/products">Product</a></li>
+					<li><a href="/suppliers">Supplier</a></li>
+					<li><a href="/transactions">Transaction</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</header>
+<!-- END OF HEADER -->
+
+<!-- START OF CONTENT -->
+<section class="body">
+	<div class="container">
+		<div class="form_content">
+			<p class="form_title">New User</p>
+			<form action="" method="">
+				<div>
+					<label>Name:</label>
+					<input type="text" name="name" value="" placeholder="">
+				</div>
+				<div>
+					<label>Email:</label>
+					<input type="text" name="email">
+				</div>
+				<div>
+					<label>Password:</label>
+					<input type="password" name="password">
+				</div>
+				<div>
+					<label>Confirm password:</label>
+					<input type="password" name="confirmation_password">
+				</div>
+				<div>
+					<label>Profile's photo:</label>
+					<input type="file" name="photo">
+				</div>
+				<div>
+					<label></label>
+					<button type="submit" name="btnAdd"><i class="fas fa-user-plus"></i> Add new user</button>
+				</div>
+			</form>
+		</div>
+		<div class="list_content">
+			<div class="list_content_row">
+				<div class="list_content_row_col">
+					<span>No</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>ID</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>Name</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>Email</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>Edit</span>
+				</div>
+			</div>
+			<div class="list_content_row">
+				<div class="list_content_row_col">
+					<span>1</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>10000</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>Ngo Dinh Cuong</span>
+				</div>
+				<div class="list_content_row_col">
+					<span>cuong.ngo@asiantech.vn</span>
+				</div>
+				<div class="list_content_row_col">
+					<a href="" class="btn btnUpdate"><i class="far fa-edit"></i></a>
+					<a href="" class="btn btnDelete"><i class="far fa-trash-alt"></i></a>
+					<a href="" class="btn btnReset"><i class="fas fa-sync-alt"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END OF CONTENT -->
+
+<!-- START OF FOOTER -->
+<footer class="footer">
+	<div class="container clearfix">
+		<div class="footer__left">
+			Designed by me
+		</div>
+		<div class="footer__right">
+			<ul>
+				<li><a href="">About us</a></li>
+				<li><a href="">Contact us</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+<!-- END OF FOOTER -->
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,128 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="vn">
+<head>
+    <title>iShop::Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css" >
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+</head>
+<body>
+<!-- START OF HEADER -->
+<header class="header">
+    <div class="container clearfix">
+        <div class="header__left">
+            <a href="" class="header__left_logo">
+                <i class="fab fa-skyatlas fa-lg"></i>
+            </a>
+        </div>
+        <div class="header__right">
+            <nav class="header__right_menu">
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/users">User</a></li>
+                    <li><a href="/shops">Shop</a></li>
+                    <li><a href="/categories">Category</a></li>
+                    <li><a href="/products">Product</a></li>
+                    <li><a href="/suppliers">Supplier</a></li>
+                    <li><a href="/transactions">Transaction</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+<!-- END OF HEADER -->
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+<!-- START OF CONTENT -->
+<section class="body">
+    <div class="container">
+        <div class="form_content">
+            <p class="form_title">New User</p>
+            <form action="" method="">
+                <div>
+                    <label>Name:</label>
+                    <input type="text" name="name" value="" placeholder="">
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div>
+                    <label>Email:</label>
+                    <input type="text" name="email">
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+                    <label>Password:</label>
+                    <input type="password" name="password">
+                </div>
+                <div>
+                    <label>Confirm password:</label>
+                    <input type="password" name="confirmation_password">
+                </div>
+                <div>
+                    <label>Profile's photo:</label>
+                    <input type="file" name="photo">
+                </div>
+                <div>
+                    <label></label>
+                    <button type="submit" name="btnAdd"><i class="fas fa-user-plus"></i> Add new user</button>
+                </div>
+            </form>
+        </div>
+        <div class="list_content">
+            <div class="list_content_row">
+                <div class="list_content_row_col">
+                    <span>No</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>ID</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>Name</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>Email</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>Edit</span>
+                </div>
+            </div>
+            <div class="list_content_row">
+                <div class="list_content_row_col">
+                    <span>1</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>10000</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>Ngo Dinh Cuong</span>
+                </div>
+                <div class="list_content_row_col">
+                    <span>cuong.ngo@asiantech.vn</span>
+                </div>
+                <div class="list_content_row_col">
+                    <a href="" class="btn btnUpdate"><i class="far fa-edit"></i></a>
+                    <a href="" class="btn btnDelete"><i class="far fa-trash-alt"></i></a>
+                    <a href="" class="btn btnReset"><i class="fas fa-sync-alt"></i></a>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
+</section>
+<!-- END OF CONTENT -->
+
+<!-- START OF FOOTER -->
+<footer class="footer">
+    <div class="container clearfix">
+        <div class="footer__left">
+            Designed by me
+        </div>
+        <div class="footer__right">
+            <ul>
+                <li><a href="">About us</a></li>
+                <li><a href="">Contact us</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+<!-- END OF FOOTER -->
+</body>
 </html>
